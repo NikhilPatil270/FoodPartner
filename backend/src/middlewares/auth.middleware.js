a26d0674ff482128ed7e2814c76186f501ace1f9
NikhilPatil270 <nikhilpp2704@gmail.com>
2025-10-28 20:51:48 +0530

    create foodItem

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,9 +1,25 @@
 const foodPartnerModel = require("../models/foodpartner.model");
 const jwt = require("jsonwebtoken");
 
-async function authFoodPartnerMiddleware(req,res,next) {
-    const token=req.cookoies.token;
-    if(!token){
-        return res.status(401).send({message:"Please login first"});
+async function authFoodPartnerMiddleware(req, res, next) {
+  const token = req.cookies.token; //get token from cookies
+  if (!token) {
+    return res.status(401).send({ message: "Please login first" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET); //verify token
+    const foodpartner = await foodPartnerModel.findById(decoded.id);
+    if (!foodpartner) {
+      return res.status(401).send({ message: "Food partner not found" });
     }
+    req.foodpartner = foodpartner; //attach foodpartner to response object
+    next();
+  } catch (error) {
+    return res.status(401).send({ message: "Invalid token" });
+  }
+}
+
+module.exports={
+    authFoodPartnerMiddleware,
 }
--- backend/src/routes/food.routes.js
+++ backend/src/routes/food.routes.js
@@ -1,6 +1,17 @@
+
 const express=require("express");
+const foodController=require('../controllers/food.controller');
+const authMiddleware=require('../middlewares/auth.middleware');
 const router=express.Router();
 
-router.post('/')
+//express server cant read files(img,vid,pdf) directly so we use multer
+const multer = require('multer');
+// to save videos/images in memory buffer
+const upload=multer({
+    storage:multer.memoryStorage(),
+})
+
+// post /api/food/ [protected]
+router.post('/',authMiddleware.authFoodPartnerMiddleware,upload.single("video"),foodController.createFood);//pehle auth krwaya fir food create krwaya
 
 module.exports=router;
